b127a0c58bd2b23420b16838ec8760f783aea519
Adem Mrizak <adem.mrizak@gmail.com>
2021-01-02 14:41:27 +0100

    feature/add empty rental list test

--- src/test/java/com/esgi/StatementTest.java
+++ src/test/java/com/esgi/StatementTest.java
@@ -4,10 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class StatementTest {
+    private final Statement statement = new Statement();
+    private final String name = "Odessa";
+
     @Test
     public void Should_GenerateStatement_When_MethodIsUsed() {
-        String name = "Odessa";
-        Customer customer = new Customer(name);
+        Customer customer = new Customer(this.name);
         customer.addRental(new Rental(
                 new Movie("Astérix", MovieType.CHILDREN),
                 6));
@@ -18,14 +20,22 @@ public class StatementTest {
                 new Movie("OSS 117 Alerte Rouge en Afrique noire", MovieType.NEW_RELEASE),
                 1));
 
-        Statement statement = new Statement();
-
         Assert.assertEquals(
-                "Rental Record for " + name + "\n" +
+                "Rental Record for " + this.name + "\n" +
                         "\tAstérix\t6.0\n" +
                         "\tPulp Fiction\t2.0\n" +
                         "\tOSS 117 Alerte Rouge en Afrique noire\t3.0\n" +
                         "You owed 11.0\n" +
-                        "You earned 3 frequent renter points\n", statement.generateStatement(customer));
+                        "You earned 3 frequent renter points\n", this.statement.generateStatement(customer));
+    }
+
+    @Test
+    public void Should_GenerateEmptyStatement_When_MethodIsUsed() {
+        Customer customer = new Customer(this.name);
+
+        Assert.assertEquals(
+                "Rental Record for " + this.name + "\n" +
+                        "You owed 0.0\n" +
+                        "You earned 0 frequent renter points\n", this.statement.generateStatement(customer));
     }
 }
